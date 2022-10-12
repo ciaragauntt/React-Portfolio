308b4b822ddfe13a0a2fa71b80e17a60ff621827
cici <ci.gauntt@gmail.com>
2022-10-12 17:22:48 -0500

    added to the projects.js

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,46 @@
 import React from "react";
 import { projects } from "../data";
 
-return (
-    <div className = "project">
-        <h1>Apps I've Built</h1>
-        <p
-    </div>
-)
+export default function Projects() {
+    return (
+        <section id="projects" className =" text-gray-400 body-font">
+            <div className="conatiner">
+                <div className ="flex">
+                    <h1 className =" title-font text-white">
+                        Projects
+                    </h1>
+                    <p>
+                        Here you will find all of the projects I have worked on 
+                        over the past 6 months. Enjoy!
+                    </p>
+                </div>
+                <div className = "flex flex-wrap">
+                    {projects.map((project) => (
+                        <a
+                            href={project.link}
+                            key={project.image}
+                            className = "sm: w-1/2 w-100 p-4">
+                            <div className = "felx relative">
+                                <img 
+                                    alt="project-gallery"
+                                    src={project.image}
+                                />
+                                <div>
+                                    <h2>
+                                        {project.subtitle}
+                                    </h2>
+                                    <h1>
+                                        {project.title}
+                                    </h1>
+                                    <p>
+                                        {project.description}
+                                    </p>
+                                </div>
+                            </div>
+                            </a>
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
